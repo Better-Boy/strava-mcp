20aee4a97af8a67e4e511619316202ce2157e2dd
Better-Boy <panaroma365@gmail.com>
2025-09-10 06:19:37 +0000

    added dotenv

--- src/strava_server/server.py
+++ src/strava_server/server.py
@@ -2,7 +2,10 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastmcp import FastMCP
+from dotenv import load_dotenv
 from .api import router
+
+load_dotenv()
 
 @asynccontextmanager
 async def app_lifespan(app: FastAPI):
